0e558ed9a8d45afa305f5245b05442620a0f29d9
Fioshi <guilhermefiochi@outlook.com>
2024-06-06 09:33:55 -0300

    feat: criados classes de conexão e uso de serviços externos

--- src/main/java/Fioshi/com/github/PicPaySimplificado/service/AuthorizationService.java
+++ src/main/java/Fioshi/com/github/PicPaySimplificado/service/AuthorizationService.java
@@ -1,4 +1,26 @@
 package Fioshi.com.github.PicPaySimplificado.service;
 
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@Service
 public class AuthorizationService {
+
+    public String authorization() throws IOException{
+
+            URL url = new URL("https://util.devi.tools/api/v2/authorize");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                return "Success";
+            } else {
+                return "Error";
+        }
+    }
+
 }
--- src/main/java/Fioshi/com/github/PicPaySimplificado/service/NotifyService.java
+++ src/main/java/Fioshi/com/github/PicPaySimplificado/service/NotifyService.java
@@ -1,4 +1,47 @@
 package Fioshi.com.github.PicPaySimplificado.service;
 
+import Fioshi.com.github.PicPaySimplificado.domain.model.User.User;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@Service
 public class NotifyService {
+
+    public String notifyng(User user) throws IOException {
+        String urlStr = "https://util.devi.tools/api/v1/notify";
+        try {
+            int responseCode = getResponseCode(urlStr);
+            if (responseCode == HttpURLConnection.HTTP_OK ||
+                    responseCode == HttpURLConnection.HTTP_CREATED ||
+                    responseCode == HttpURLConnection.HTTP_NO_CONTENT) {
+                return "Notificação enviada para: " + user.getName() + " " + user.getSurname()
+                        ;
+            } else {
+                return "O serviço falhou";
+            }
+        } catch (IOException e) {
+            return "Failed to send notification due to an error: " + e.getMessage();
+        }
+    }
+
+    private static int getResponseCode(String urlStr) throws IOException {
+        URL url = new URL(urlStr);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json; utf-8");
+        connection.setDoOutput(true);
+
+        String jsonInputString = "{\"message\":\""  + "\"}";
+        try (OutputStream os = connection.getOutputStream()) {
+            byte[] input = jsonInputString.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+
+        int responseCode = connection.getResponseCode();
+        return responseCode;
+    }
 }
