3455dc3310351e9cfbe368a1266569e2575caa0c
Fioshi <guilhermefiochi@outlook.com>
2024-06-06 09:30:09 -0300

    feat: criado bean dos validadores de pagamento

---
+++ src/main/java/Fioshi/com/github/PicPaySimplificado/configuration/BeanValidations.java
@@ -0,0 +1,19 @@
+package Fioshi.com.github.PicPaySimplificado.configuration;
+
+import Fioshi.com.github.PicPaySimplificado.domain.model.Payment.validations.PaymentValidation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+@Configuration
+public class BeanValidations {
+
+    @Bean
+    public List<PaymentValidation> myList() {
+        return new LinkedList<>();
+    }
+
+}
