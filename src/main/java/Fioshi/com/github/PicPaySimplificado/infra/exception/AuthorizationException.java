51b9ae9bac8cedb8ec850d92a4a4a195d1b4e110
Fioshi <guilhermefiochi@outlook.com>
2024-06-07 09:18:36 -0300

    feat: criadas classes de Exception personalizadas

---
+++ src/main/java/Fioshi/com/github/PicPaySimplificado/infra/exception/AuthorizationException.java
@@ -0,0 +1,9 @@
+package Fioshi.com.github.PicPaySimplificado.infra.exception;
+
+public class AuthorizationException extends RuntimeException{
+
+    public AuthorizationException(String message){
+        super(message);
+    }
+
+}
---
+++ src/main/java/Fioshi/com/github/PicPaySimplificado/infra/exception/ErrorTreatment.java
@@ -0,0 +1,14 @@
+package Fioshi.com.github.PicPaySimplificado.infra.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorTreatment {
+
+    @ExceptionHandler(AuthorizationException.class)
+    public ResponseEntity tratarErroRegraDeNegocio(AuthorizationException ex){
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+}
