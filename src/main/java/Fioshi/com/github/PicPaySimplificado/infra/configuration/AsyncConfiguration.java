105831f82f9a9663b6dd1d23341f68cccfb87a37
Fioshi <guilhermefiochi@outlook.com>
2024-07-08 13:44:02 -0300

    rerf: Criado arquivo de configuração de threads para pagamento

---
+++ src/main/java/Fioshi/com/github/PicPaySimplificado/infra/configuration/AsyncConfiguration.java
@@ -0,0 +1,23 @@
+package Fioshi.com.github.PicPaySimplificado.infra.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class AsyncConfiguration {
+
+    @Bean
+    public Executor asyncTransaction(){
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(15);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("AsynchThreadTransaction-");
+        executor.initialize();
+        return executor;
+    }
+
+}
