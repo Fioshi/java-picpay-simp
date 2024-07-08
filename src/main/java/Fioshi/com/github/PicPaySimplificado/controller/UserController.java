d83c952c13786afd15ec93c49bf15784550c3eae
Fioshi <guilhermefiochi@outlook.com>
2024-07-08 15:37:24 -0300

    build: adicionado metodo de login e modificado metodo de cadastro

--- src/main/java/Fioshi/com/github/PicPaySimplificado/controller/UserController.java
+++ src/main/java/Fioshi/com/github/PicPaySimplificado/controller/UserController.java
@@ -1,9 +1,16 @@
 package Fioshi.com.github.PicPaySimplificado.controller;
 
-import Fioshi.com.github.PicPaySimplificado.domain.model.User.UserDTO;
+import Fioshi.com.github.PicPaySimplificado.domain.model.User.User;
+import Fioshi.com.github.PicPaySimplificado.domain.model.User.UserDto;
+import Fioshi.com.github.PicPaySimplificado.domain.model.User.UserLoginDto;
+import Fioshi.com.github.PicPaySimplificado.domain.repository.UserRepository;
+import Fioshi.com.github.PicPaySimplificado.service.TokenService;
 import Fioshi.com.github.PicPaySimplificado.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +23,26 @@ import java.io.IOException;
 public class UserController {
 
     @Autowired
+    private AuthenticationManager manager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
     private UserService userService;
 
-    @PostMapping
-    public ResponseEntity<String> insertUser(@RequestBody UserDTO dto) throws IOException {
+    @PostMapping("subscribe")
+    public ResponseEntity<String> subscribe(@RequestBody UserDto dto) throws IOException {
         userService.insert(dto);
         return ResponseEntity.ok("Criado com sucesso");
     }
 
+    @PostMapping("login")
+    public ResponseEntity<String> login(@RequestBody @Valid UserLoginDto dto) {
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dto. email(), dto.password());
+        var authentication = manager.authenticate(authenticationToken);
+
+        var tokenJWT = tokenService.gerarToken((User) authentication.getPrincipal());
+        return ResponseEntity.ok(tokenJWT);
+    }
 }
