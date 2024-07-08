aa9ece115ea64586a339f091ea11423f96a73e39
Fioshi <guilhermefiochi@outlook.com>
2024-07-08 15:33:25 -0300

    feat: habilitando assincrono

--- src/main/java/Fioshi/com/github/PicPaySimplificado/PicPaySimplificadoApplication.java
+++ src/main/java/Fioshi/com/github/PicPaySimplificado/PicPaySimplificadoApplication.java
@@ -2,7 +2,10 @@ package Fioshi.com.github.PicPaySimplificado;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableAsync
 @SpringBootApplication
 public class PicPaySimplificadoApplication {
 
