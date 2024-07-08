c1c6803226db1d1d044332bb290ccab19340aa58
Fioshi <guilhermefiochi@outlook.com>
2024-07-08 15:32:41 -0300

    feat: criado metodo para consulta por email

--- src/main/java/Fioshi/com/github/PicPaySimplificado/domain/repository/UserRepository.java
+++ src/main/java/Fioshi/com/github/PicPaySimplificado/domain/repository/UserRepository.java
@@ -4,4 +4,5 @@ import Fioshi.com.github.PicPaySimplificado.domain.model.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String subject);
 }
