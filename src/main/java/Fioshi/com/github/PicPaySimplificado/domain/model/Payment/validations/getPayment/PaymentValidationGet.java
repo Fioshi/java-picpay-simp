1a70d90624d27ae0362b5dbe1906c07b395ebbac
Fioshi <guilhermefiochi@outlook.com>
2024-07-10 12:07:46 -0300

    refactor: retiradas classes de validação de metodos get

--- src/main/java/Fioshi/com/github/PicPaySimplificado/domain/model/Payment/validations/getPayment/PaymentValidationGet.java
+++
@@ -1,9 +0,0 @@
-package Fioshi.com.github.PicPaySimplificado.domain.model.Payment.validations.getPayment;
-
-import Fioshi.com.github.PicPaySimplificado.domain.model.Account.Account;
-
-public interface PaymentValidationGet {
-
-    void validation(Account payer);
-
-}
