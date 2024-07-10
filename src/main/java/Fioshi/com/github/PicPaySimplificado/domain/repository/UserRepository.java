c3906106e7a225326782fbd2f2729ffaee4e42ee
Fioshi <guilhermefiochi@outlook.com>
2024-07-10 13:06:00 -0300

    refactor: mudança no nome de variavel

--- src/main/java/Fioshi/com/github/PicPaySimplificado/domain/repository/UserRepository.java
+++ src/main/java/Fioshi/com/github/PicPaySimplificado/domain/repository/UserRepository.java
@@ -4,5 +4,5 @@ import Fioshi.com.github.PicPaySimplificado.domain.model.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String subject);
+    User findByEmail(String email);
 }
